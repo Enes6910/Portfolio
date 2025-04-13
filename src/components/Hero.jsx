636a8167a398047c0e6c012b164a41b999a4abf2
Enes6910 <enes.saglam.bts@gmail.com>
2025-04-14 01:39:50 +0200

    2nd commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Hero = () => (
   <section className="hero">
     <h2>Salut, moi c’est <span className="highlight">Enes</span> 👋</h2>
-    <p>Développeur passionné par les interfaces modernes.</p>
+    <p>Développeur junior en plein parcours scolaire, passioné par l'informatique.</p>
   </section>
 );
 
